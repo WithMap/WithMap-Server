a7b158b83f8b2971ba51ca4e9562db37498972bc
최선재 <homesking@naver.com>
2019-09-24 20:56:16 +0900

    add constructor in pin class

--- src/main/java/com/seoulapp/withmap/model/Pin.java
+++ src/main/java/com/seoulapp/withmap/model/Pin.java
@@ -1,12 +1,16 @@
 package com.seoulapp.withmap.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Pin extends Dto {
 
 	private int id;
