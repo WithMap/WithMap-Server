7f1f955debc2d32a4b4ec40620dc1c236292b37d
최선재 <homesking@naver.com>
2019-09-27 15:46:16 +0900

    crtdate, upddate -> string 형태로 반환 수정

--- src/main/java/com/seoulapp/withmap/model/Dto.java
+++ src/main/java/com/seoulapp/withmap/model/Dto.java
@@ -10,5 +10,13 @@ import lombok.Setter;
 public class Dto {
 	private DateTime crtDate;
 	private DateTime updDate;
+	
+	public String getCrtDate() {
+		return crtDate.toString();
+	}
+	
+	public String getUpdDate() {
+		return updDate.toString();
+	}
 }
 
